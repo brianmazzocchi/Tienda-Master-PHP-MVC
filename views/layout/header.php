7ebf5a8eafefe23b3c12dfb552adf59f9a3767bc
brianmazzocchi <66741736+brianmazzocchi@users.noreply.github.com>
2021-11-04 11:00:40 -0300

    Categorias done

--- views/layout/header.php
+++ views/layout/header.php
@@ -21,6 +21,7 @@
         </header>
 
         <!-- Menu -->
+        <?php $categorias = Utils::showCategorias(); ?>
         <nav id="menu">
             <ul>
                 <li>
@@ -28,31 +29,11 @@
                         Inicio
                     </a>
                 </li>
-                <li>
-                    <a href="#">
-                        Categoria 1
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Categoria 2
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Categoria 3
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Categoria 4
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        Categoria 5
-                    </a>
-                </li>
+                <?php while($cat = $categorias->fetch_object()): ?>
+                    <li>
+                        <a href="#"><?=$cat->nombre?></a>
+                    </li>                
+                <?php endwhile; ?>             
             </ul>
         </nav>
 
